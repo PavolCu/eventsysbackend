b2c82ccffda0260511fba559f19f78e5a0d03548
Pavol Cuninka <pavol.cuninka@icloud.com>
2024-04-29 23:30:41 +0200

    commiting established spring for backand aplication

---
+++ src/main/java/com/eventssystem/eventsysbackend/EventsysbackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventssystem.eventsysbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsysbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsysbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventssystem/eventsysbackend/EventsysbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventssystem.eventsysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsysbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
